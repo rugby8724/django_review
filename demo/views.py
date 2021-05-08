9d3d1aea8d7b588b249ff63ecfd96f6ef7ae41a0
Jeremiah <jeremiah.d.wise@gmail.com>
2021-05-08 13:07:26 -0500

    Add database to demo

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
-# Create your views here.
+
+# class Another(View):
+#
+#     books = Book.objects.filter(is_published=True)
+#     output = ''
+#
+#     for book in books:
+#         output += f'We have {book.title} book with an ID of {book.id}<br>'
+#
+#     def get(self, request):
+#         return HttpResponse(self.output)
+
+
+def first(request):
+    return render(request, 'index.html')
--- first-project/settings.py
+++ first-project/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'first-project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
